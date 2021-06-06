d8bd18acf34481bf02d8626ae26d1fc568eb0e15
Mark Sale <msale2@yahoo.com>
2021-06-05 21:06:56 -0700

    Delete process.py

--- process.py
+++
@@ -1,6 +0,0 @@
-from subprocess import Popen, PIPE
-
-def runNONMEM:
-    process = Popen(['echo', 'test.py'], stdout=PIPE, stderr=PIPE)
-    stdout, stderr = process.communicate()
-    print stdout
